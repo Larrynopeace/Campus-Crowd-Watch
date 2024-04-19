4f7fccdd7762e3a580ece670c7ce722af92f71c6
chele691 <chele691@student.otago.ac.nz>
2024-04-19 13:03:59 +1200

    /occupancy/:building

--- Backend-Express-MongoDB/MongoDB/Routes/Occupancy/OccupancyRoute.js
+++ Backend-Express-MongoDB/MongoDB/Routes/Occupancy/OccupancyRoute.js
@@ -8,13 +8,26 @@ const router = Router();
 
 // Get all occupancy data
 router.get("/api/occupancy", async (_, res) => {
-    try {
-        const occupancyData = await OccupancyModel.find({}).populate('building_id');
-        res.send(occupancyData);
-    } catch (err) {
-        console.error("Error occurred:", err);
-        res.status(500).send("Internal Server Error");
-    }
+  try {
+    const occupancyData = await OccupancyModel.find({}).populate("building_id");
+    res.send(occupancyData);
+  } catch (err) {
+    console.error("Error occurred:", err);
+    res.status(500).send("Internal Server Error");
+  }
 });
-
+router.get("/api/occupancy/:building", async (req, res) => {
+  try {
+    const user = await UserModel.findOne({
+      building: req.params.favorite_buildings.building_id,
+    }).populate("favorite_buildings");
+    if (!user) {
+      return res.status(404).send("user not found");
+    }
+    res.send(user);
+  } catch (err) {
+    console.error("Error occurred:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
 export default router;
