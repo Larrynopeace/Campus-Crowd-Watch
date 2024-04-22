8c269c6a979c0a2891c608b0295f10d6165f5c50
Weizhuo Chan <2501990530@qq.com>
2024-04-22 15:15:44 +1200

    Created an testing example about displaying a drawer and a bar chart by clicking a card

---
+++ Frontend-Vue/pages/charts-and-drawer/js/taskExample.js
@@ -0,0 +1,45 @@
+// This is the line chart controller
+const { createApp, ref, onMounted } = Vue
+
+const app = createApp({
+    setup() {
+        // Declare a reactive variable
+        const message = ref('Hello vue!')
+        // Declare a function to modify the reactive variable
+        const addLetter = () => {
+            message.value += 'ABC'
+        }
+
+        // This is the controller of the drawer of graph
+        const table = ref(false)
+        // This is the method to open the drawer
+        const openTable = () => {
+            // Modify the reactive variable to open the drawer
+            table.value = true
+        }
+
+        // This function will be called when the component is mounted
+        onMounted(() => {
+            console.log('Component is mounted')
+        })
+
+        // Return the reactive variable and the function so that they can be accessed in the template
+        return {
+            // Variable
+            message,
+            table,
+
+            // Function
+            openTable,
+            addLetter,
+        }
+    }
+})
+
+import BarChart from './barChartExample.js';   
+// Register the component
+app.component('bar-chart', BarChart);
+
+app.use(ElementPlus);
+app.mount('#app');
+
